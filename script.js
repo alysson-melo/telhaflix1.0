b0b1dc04e0e9a854ce83801830cc6d4e7de4811c
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-04-15 16:42:21 -0300

    v1.1

---
+++ script.js
@@ -0,0 +1,19 @@
+if (window.innerWidth <= 768) {
+    const nav = document.querySelector('#nav')
+    const header = document.querySelector('.header')
+    const barra = document.querySelector('.barra')
+    const menu = document.querySelector('.menu')
+    const logoTv = document.querySelector('.logo-tv')
+
+    nav.addEventListener('click', clickNav)
+    nav.addEventListener('touchstart', clickNav)
+
+    function clickNav(event) {
+        if (event.type === 'touchstart') event.preventDefault()
+        header.classList.toggle('header-active')
+        barra.classList.toggle('barra-active')
+        menu.classList.toggle('menu-active')
+        logoTv.classList.toggle('logo-tv-active')
+        logoTv.classList.toggle('logo-tv')
+    }
+}
