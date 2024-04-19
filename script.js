a59162fa64ae1b029c482f39e65ec855d58272bf
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-04-19 18:18:10 -0300

    v3.0

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ if (window.innerWidth <= 768) {
     const logoTv = document.querySelector('.logo-tv')
 
     nav.addEventListener('click', clickNav)
+    nav.addEventListener('touchstart', clickNav)
 
-    function clickNav() {
+    function clickNav(event) {
+        if (event.type === 'touchstart') event.preventDefault()
         header.classList.toggle('header-active')
         barra.classList.toggle('barra-active')
         menu.classList.toggle('menu-active')
@@ -15,25 +17,3 @@ if (window.innerWidth <= 768) {
         logoTv.classList.toggle('logo-tv')
     }
 }
-
-
-
-// if (window.innerWidth <= 768) {
-//     const nav = document.querySelector('.nav-menu')
-//     const header = document.querySelector('.header')
-//     const barra = document.querySelector('.barra')
-//     const menu = document.querySelector('.menu')
-//     const logoTv = document.querySelector('.logo-tv')
-
-//     nav.addEventListener('click', clickNav)
-//     nav.addEventListener('touchstart', clickNav)
-
-//     function clickNav(event) {
-//         if (event.type === 'touchstart') event.preventDefault()
-//         header.classList.toggle('header-active')
-//         barra.classList.toggle('barra-active')
-//         menu.classList.toggle('menu-active')
-//         logoTv.classList.toggle('logo-tv-active')
-//         logoTv.classList.toggle('logo-tv')
-//     }
-// }
