dac63a3fac0851d816f28868c9d59d891f646b5a
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-04-20 23:39:49 -0300

    v3.1

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 if (window.innerWidth <= 768) {
-    const nav = document.querySelector('.nav-menu')
-    const header = document.querySelector('.header')
+    const hamburger = document.querySelector('.hamburguer')
     const barra = document.querySelector('.barra')
     const menu = document.querySelector('.menu')
-    const logoTv = document.querySelector('.logo-tv')
-
-    nav.addEventListener('click', clickNav)
-    nav.addEventListener('touchstart', clickNav)
+    hamburger.addEventListener('click', clickNav)
+    hamburger.addEventListener('touchstart', clickNav)
 
     function clickNav(event) {
         if (event.type === 'touchstart') event.preventDefault()
-        header.classList.toggle('header-active')
         barra.classList.toggle('barra-active')
         menu.classList.toggle('menu-active')
-        logoTv.classList.toggle('logo-tv-active')
-        logoTv.classList.toggle('logo-tv')
+        hamburger.classList.toggle('hamburger-active')
     }
 }
