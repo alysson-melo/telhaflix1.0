620b791ababc4e24b3256ab5ce7009c8b5cb4263
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-04-21 09:48:13 -0300

    v3.2

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ if (window.innerWidth <= 768) {
     const hamburger = document.querySelector('.hamburguer')
     const barra = document.querySelector('.barra')
     const menu = document.querySelector('.menu')
-    hamburger.addEventListener('click', clickNav)
     hamburger.addEventListener('touchstart', clickNav)
 
-    function clickNav(event) {
-        if (event.type === 'touchstart') event.preventDefault()
+    function clickNav() {
         barra.classList.toggle('barra-active')
         menu.classList.toggle('menu-active')
         hamburger.classList.toggle('hamburger-active')
     }
 }
+
+// if (window.innerWidth <= 768) {
+//     const hamburger = document.querySelector('.hamburguer')
+//     const barra = document.querySelector('.barra')
+//     const menu = document.querySelector('.menu')
+//     hamburger.addEventListener('click', clickNav)
+//     hamburger.addEventListener('touchstart', clickNav)
+
+//     function clickNav(event) {
+//         if (event.type === 'touchstart') event.preventDefault()
+//         barra.classList.toggle('barra-active')
+//         menu.classList.toggle('menu-active')
+//         hamburger.classList.toggle('hamburger-active')
+//     }
+// }
