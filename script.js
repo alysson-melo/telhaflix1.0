5e38c724fd398001f7753c17815ac07edd168bdc
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-05-18 23:45:08 -0300

    v5.10

--- script.js
+++ script.js
@@ -4,129 +4,130 @@
 
 const loadScreen = document.querySelector('.screen-load')
 
-document.getElementById("trailer").addEventListener('loadedmetadata', () => {
+if (window.location.pathname === 'https://alysson-melo.github.io/telhaflix/index.html' || window.location.pathname === 'https://alysson-melo.github.io/telhaflix/') {
 
-    loadScreen.classList.add('off')
-    setTimeout(() => {
-        loadScreen.classList.add('d-none')
-    }, 600)
+    document.getElementById("trailer").addEventListener('loadedmetadata', () => {
 
-    // ---------------------
-    // TRAILER HOME
-    // ---------------------
+        loadScreen.classList.add('off')
+        setTimeout(() => {
+            loadScreen.classList.add('d-none')
+        }, 600)
 
-    if (window.innerWidth >= 768) {
+        // ---------------------
+        // TRAILER HOME
+        // ---------------------
 
-        function trailerOn() {
+        if (window.innerWidth >= 768) {
 
-            const banner = document.querySelector('.banner')
-            const trailer = document.querySelector('.trailer-off')
+            function trailerOn() {
 
-            banner.classList.add('banner-off')
-            trailer.classList.remove('trailer-off')
-            trailer.classList.add('trailer-on')
+                const banner = document.querySelector('.banner')
+                const trailer = document.querySelector('.trailer-off')
 
-            trailer.currentTime = 0
-            trailer.volume = 0
+                banner.classList.add('banner-off')
+                trailer.classList.remove('trailer-off')
+                trailer.classList.add('trailer-on')
 
-            const bntMute = document.querySelector('.btn-mute')
-            bntMute.classList.add('btn-mute-active')
-            bntMute.addEventListener('click', mute)
-            bntMute.innerHTML = 'volume_off'
+                trailer.currentTime = 0
+                trailer.volume = 0
 
-            function mute() {
-                if (trailer.volume == 0) {
-                    trailer.volume = 1
-                    bntMute.innerHTML = 'volume_up'
-                } else {
-                    trailer.volume = 0
-                    bntMute.innerHTML = 'volume_off'
-                }
-            }
+                const bntMute = document.querySelector('.btn-mute')
+                bntMute.classList.add('btn-mute-active')
+                bntMute.addEventListener('click', mute)
+                bntMute.innerHTML = 'volume_off'
 
-            trailer.play()
-
-            trailer.addEventListener('ended', () => {
-                trailerOff()
-            })
-        }
-
-        function trailerOff() {
-            const banner = document.querySelector('.banner')
-            const trailer = document.querySelector('.trailer-on')
-            const bntMute = document.querySelector('.btn-mute')
-
-            if (document.querySelector('.trailer-on')) {
-                const fadeAudio = setInterval(() => {
-                    if (trailer.volume > 0.10) {
-                        trailer.volume -= 0.10
-                    } else {
-                        clearInterval(fadeAudio)
-                        trailer.pause()
+                function mute() {
+                    if (trailer.volume == 0) {
                         trailer.volume = 1
-                        banner.classList.remove('banner-off')
-                        trailer.classList.remove('trailer-on')
-                        trailer.classList.add('trailer-off')
-                        bntMute.classList.remove('btn-mute-active')
-                        bntMute.addEventListener('click', mute)
-
-                        function mute() {
-                            if (trailer.volume == 0) {
-                                trailer.volume = 1
-                                bntMute.innerHTML = 'volume_up'
-                            } else {
-                                trailer.volume = 0
-                                bntMute.innerHTML = 'volume_off'
-                            }
-                        }
-
+                        bntMute.innerHTML = 'volume_up'
+                    } else {
+                        trailer.volume = 0
+                        bntMute.innerHTML = 'volume_off'
                     }
-                }, 80)
-            }
-        }
-
-        let timer
-
-        function handleIntersection(entries, observer) {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    timer = setTimeout(() => {
-                        trailerOn()
-                    }, 3000)
-                } else {
-                    clearTimeout(timer)
-                    trailerOff()
                 }
-            })
-        }
 
-        if (document.querySelector('.banner')) {
+                trailer.play()
+
+                trailer.addEventListener('ended', () => {
+                    trailerOff()
+                })
+            }
+
+            function trailerOff() {
+                const banner = document.querySelector('.banner')
+                const trailer = document.querySelector('.trailer-on')
+                const bntMute = document.querySelector('.btn-mute')
+
+                if (document.querySelector('.trailer-on')) {
+                    const fadeAudio = setInterval(() => {
+                        if (trailer.volume > 0.10) {
+                            trailer.volume -= 0.10
+                        } else {
+                            clearInterval(fadeAudio)
+                            trailer.pause()
+                            trailer.volume = 1
+                            banner.classList.remove('banner-off')
+                            trailer.classList.remove('trailer-on')
+                            trailer.classList.add('trailer-off')
+                            bntMute.classList.remove('btn-mute-active')
+                            bntMute.addEventListener('click', mute)
+
+                            function mute() {
+                                if (trailer.volume == 0) {
+                                    trailer.volume = 1
+                                    bntMute.innerHTML = 'volume_up'
+                                } else {
+                                    trailer.volume = 0
+                                    bntMute.innerHTML = 'volume_off'
+                                }
+                            }
+
+                        }
+                    }, 80)
+                }
+            }
+
+            let timer
+
+            function handleIntersection(entries, observer) {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        timer = setTimeout(() => {
+                            trailerOn()
+                        }, 3000)
+                    } else {
+                        clearTimeout(timer)
+                        trailerOff()
+                    }
+                })
+            }
+
             const bannerObserver = new IntersectionObserver(handleIntersection)
             const banner = document.querySelector('.banner')
             bannerObserver.observe(banner)
         }
-    }
 
-    // ---------------------
-    // MENU
-    // ---------------------
+        // ---------------------
+        // MENU
+        // ---------------------
 
-    if (window.innerWidth <= 768) {
+        if (window.innerWidth <= 768) {
 
-        loadScreen.classList.add('off')
-        const hamburger = document.querySelector('.hamburguer')
-        const barra = document.querySelector('.barra')
-        const menu = document.querySelector('.menu')
-        hamburger.addEventListener('click', clickNav)
+            loadScreen.classList.add('off')
+            const hamburger = document.querySelector('.hamburguer')
+            const barra = document.querySelector('.barra')
+            const menu = document.querySelector('.menu')
+            hamburger.addEventListener('click', clickNav)
 
-        function clickNav() {
-            barra.classList.toggle('barra-active')
-            menu.classList.toggle('menu-active')
-            hamburger.classList.toggle('hamburger-active')
+            function clickNav() {
+                barra.classList.toggle('barra-active')
+                menu.classList.toggle('menu-active')
+                hamburger.classList.toggle('hamburger-active')
+            }
         }
-    }
 
-})
+    })
+}
 
 // ---------------------
 // BOTAO DO TRAILER
